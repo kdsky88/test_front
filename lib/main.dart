12856076089021d9935aecde4394393a081fc9b8
kdsky88 <kdsky88@gmail.com>
2026-06-12 15:28:24 +0900

    feat: 달력 날짜별 Todo 관리 기능 구현 (#37)
    - 달력 화면 추가 (calendar_screen.dart)
    - 달력 상태 관리 추가 (calendar_notifier.dart)
    - Todo API 달력 조회 연동
    - 메인 화면에 달력 탭 추가
    - Todo 폼 다이얼로그 날짜 선택 개선
    
    Closes #36

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'screens/todo_list_screen.dart';
+import 'screens/calendar_screen.dart';
 import 'state/todo_notifier.dart';
+import 'state/calendar_notifier.dart';
 
 void main() {
   runApp(const TodoApp());
@@ -14,11 +17,14 @@ class TodoApp extends StatefulWidget {
 }
 
 class _TodoAppState extends State<TodoApp> {
-  final _notifier = TodoNotifier();
+  final _todoNotifier = TodoNotifier();
+  final _calendarNotifier = CalendarNotifier();
+  int _selectedTab = 0;
 
   @override
   void dispose() {
-    _notifier.dispose();
+    _todoNotifier.dispose();
+    _calendarNotifier.dispose();
     super.dispose();
   }
 
@@ -31,7 +37,43 @@ class _TodoAppState extends State<TodoApp> {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2563EB)),
         useMaterial3: true,
       ),
-      home: TodoListScreen(notifier: _notifier),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('ko', 'KR'),
+        Locale('en', 'US'),
+      ],
+      home: Scaffold(
+        body: IndexedStack(
+          index: _selectedTab,
+          children: [
+            TodoListScreen(notifier: _todoNotifier),
+            CalendarScreen(
+              calendarNotifier: _calendarNotifier,
+              todoNotifier: _todoNotifier,
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedTab,
+          onTap: (i) => setState(() => _selectedTab = i),
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list_alt_outlined),
+              activeIcon: Icon(Icons.list_alt),
+              label: '목록',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_month_outlined),
+              activeIcon: Icon(Icons.calendar_month),
+              label: '달력',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
